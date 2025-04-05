cf0f078d9860628b6bfc49ccf432ae890399ee63
igoryas27 <neigor01@mail.ru>
2025-04-05 14:51:07 +1000

    Add files via upload

--- db/kvest_bot_dump.sql
+++ db/kvest_bot_dump.sql
@@ -1,1 +1,71 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 17.4 (Postgres.app)
+-- Dumped by pg_dump version 17.4 (Postgres.app)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    user_id bigint NOT NULL,
+    full_name text,
+    username text,
+    phone_number text,
+    quests_completed integer DEFAULT 0,
+    trophies_received integer DEFAULT 0,
+    total_points integer DEFAULT 0
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (user_id, full_name, username, phone_number, quests_completed, trophies_received, total_points) FROM stdin;
+1	Данила Ковтун	whoareyoubody	+79999999999	2	4	2300
+7629813278	ПРОЕКТОР СТРАХА	proektor_straha	\N	0	0	0
+0	Игорямба	igoryaas	-	0	0	123
+\.
+
+
+--
+-- Name: users unique_username; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT unique_username UNIQUE (username);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (user_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
 
